ac52febcbfe2096cd37e6ae4e274104bc91d6749
sampathkumar-co <sk96429643@gmail.com>
2026-08-24 21:32:47 +0530

    lexigen omega: expose portable and genesis APIs

--- lexigen-omega/src/lexigen_omega/__init__.py
+++ lexigen-omega/src/lexigen_omega/__init__.py
@@ -4,6 +4,7 @@ Development-only. Importing this package makes no AGI or breakthrough claim.
 """
 
 from .gene import CausalEvidence, GeneAdmissionPolicy, SemanticGene
+from .genesis import GeneProposal, mine_semantic_gene_proposals
 from .ir import (
     BudgetExceeded,
     ExecutionBudget,
@@ -15,6 +16,7 @@ from .ir import (
     execute_program,
     execute_program_metered,
 )
+from .portable import execute_portable, execute_portable_metered
 
 __all__ = [
     "BudgetExceeded",
@@ -22,11 +24,15 @@ __all__ = [
     "ExecutionBudget",
     "ExecutionResult",
     "GeneAdmissionPolicy",
+    "GeneProposal",
     "Instruction",
     "OpCode",
     "Program",
     "SemanticGene",
     "TypeTag",
+    "execute_portable",
+    "execute_portable_metered",
     "execute_program",
     "execute_program_metered",
+    "mine_semantic_gene_proposals",
 ]
